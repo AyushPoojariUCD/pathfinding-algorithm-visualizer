ef2b31f99352e37afb29b56ec6515bac75a571ad
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-08-01 10:34:07 +0530

    feat: added context and components

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pathfinding-algorithm-visualizer
+# Pathfinding Visualizer
+
+### React + TypeScript + Tailwind
+
+#### [Deployed app](https://pathfinding-visualizer-nu.vercel.app/)
+
+![app-demo](./src/assets/pathfinding-visualizer.gif)
+
+### Get Started From Scratch
+
+```
+❯ npm create vite@latest pathfinding-visualizer -- --template react-ts
+❯ npm install -D tailwindcss postcss autoprefixer
+❯ npx tailwindcss init -p
+❯ npm i
+❯ npm run dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { Grid } from "./components/Grid";
+import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
+import { Nav } from "./components/Nav";
 
 function App() {
+  const isVisualizationRunningRef = useRef(false);
+
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
-  )
+    <PathfindingProvider>
+      <TileProvider>
+        <SpeedProvider>
+          <div className="h-screen w-screen flex flex-col bg-[#131416]">
+            <Nav isVisualizationRunningRef={isVisualizationRunningRef} />
+            <Grid isVisualizationRunningRef={isVisualizationRunningRef} />
+          </div>
+        </SpeedProvider>
+      </TileProvider>
+    </PathfindingProvider>
+  );
 }
 
-export default App
+export default App;
